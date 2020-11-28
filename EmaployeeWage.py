8e8d9051736543d7c639b56d1a888b57de38a280
Birajit <birajit95@gmail.com>
2020-11-28 13:50:06 +0530

    [Birajit] Add: Employee Attendence is added

--- EmaployeeWage.py
+++ EmaployeeWage.py
@@ -1,4 +1,13 @@
+import random
+class Employee:
 
+    def employeeAttendance(self):
+        if random.choice([1, 0]) == 0:
+            return "present"
+        return "absent"
 
 if __name__ == "__main__":
     print("Welcome to Employee Wage Program")
+
+    emp = Employee()
+    print(emp.employeeAttendance())
