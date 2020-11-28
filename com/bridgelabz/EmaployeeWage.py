691b1eb914717dbf6afc394bf4a2f3f4b5523e6d
Birajit <birajit95@gmail.com>
2020-11-28 20:24:49 +0530

    [Birajit] Add: Daily Employee Wage is calculated

--- com/bridgelabz/EmaployeeWage.py
+++ com/bridgelabz/EmaployeeWage.py
@@ -1,13 +1,25 @@
 import random
+
+
 class Employee:
+    __WAGE_PER_HOUR = 20
+    __FULL_DAY_HOUR = 8
 
     def employeeAttendance(self):
         if random.choice([1, 0]) == 0:
             return "present"
         return "absent"
 
+    def calculateDailyEmployeeWage(self):
+        return Employee.__WAGE_PER_HOUR * Employee.__FULL_DAY_HOUR
+
+
+
 if __name__ == "__main__":
     print("Welcome to Employee Wage Program")
 
     emp = Employee()
     print(emp.employeeAttendance())
+    print(emp.calculateDailyEmployeeWage())
+
+
